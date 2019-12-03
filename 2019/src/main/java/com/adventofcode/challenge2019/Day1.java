f286df6760187b5f9154959e4a769c851b39f91c
cedricms <cedricms@gmail.com>
2019-12-03 12:09:53 +0000

    Solves the Day1 challenge.

--- 2019/src/main/java/com/adventofcode/challenge2019/Day1.java
+++ 2019/src/main/java/com/adventofcode/challenge2019/Day1.java
@@ -1,5 +1,27 @@
 package com.adventofcode.challenge2019;
 
+import java.util.List;
+
 public class Day1 extends Day {
 
+    public Long processFuelNeeds(String inputFilePath) {
+        List<Long> data = this.readFileToLongList(inputFilePath);
+
+        Long fuelNeeds = 0L;
+        for(Long moduleMass : data) {
+            Long fuelNeed = processModuleFuelNeed(moduleMass);
+
+            fuelNeeds += fuelNeed;
+        }
+
+        return fuelNeeds;
+    }
+
+    private Long processModuleFuelNeed(Long moduleMass) {
+        if (moduleMass != null) {
+            return (moduleMass / 3) - 2;
+        } else{
+            return 0L;
+        }
+    }
 }
--- 2019/src/test/java/com/adventofcode/challenge2019/Day1Test.java
+++ 2019/src/test/java/com/adventofcode/challenge2019/Day1Test.java
@@ -13,7 +13,7 @@ public class Day1Test {
     @Test
     public void readFileToLongListGivenFileThenLongList() throws Exception {
         // Given
-        String inputFilePath = "./src/test/resources/LongTestList.txt";
+        String inputFilePath = "./src/test/resources/LongTestList.data";
         Day1 day = new Day1();
 
         // When
@@ -22,5 +22,75 @@ public class Day1Test {
         // Then
         assertThat(longList).isNotNull();
         assertThat(longList.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void processFuelNeedsGiven12Then2() throws Exception {
+        // Given
+        String inputFilePath = "./src/test/resources/Day1/Day1Test1.data";
+        Day1 day = new Day1();
+
+        // When
+        Long fuelNeeds = day.processFuelNeeds(inputFilePath);
+
+        // Then
+        assertThat(fuelNeeds).isNotNull();
+        assertThat(fuelNeeds).isEqualTo(2L);
+    }
+
+    @Test
+    public void processFuelNeedsGiven14Then2() throws Exception {
+        // Given
+        String inputFilePath = "./src/test/resources/Day1/Day1Test2.data";
+        Day1 day = new Day1();
+
+        // When
+        Long fuelNeeds = day.processFuelNeeds(inputFilePath);
+
+        // Then
+        assertThat(fuelNeeds).isNotNull();
+        assertThat(fuelNeeds).isEqualTo(2L);
+    }
+
+    @Test
+    public void processFuelNeedsGiven1969Then654() throws Exception {
+        // Given
+        String inputFilePath = "./src/test/resources/Day1/Day1Test3.data";
+        Day1 day = new Day1();
+
+        // When
+        Long fuelNeeds = day.processFuelNeeds(inputFilePath);
+
+        // Then
+        assertThat(fuelNeeds).isNotNull();
+        assertThat(fuelNeeds).isEqualTo(654L);
+    }
+
+    @Test
+    public void processFuelNeedsGiven100756Then33583() throws Exception {
+        // Given
+        String inputFilePath = "./src/test/resources/Day1/Day1Test4.data";
+        Day1 day = new Day1();
+
+        // When
+        Long fuelNeeds = day.processFuelNeeds(inputFilePath);
+
+        // Then
+        assertThat(fuelNeeds).isNotNull();
+        assertThat(fuelNeeds).isEqualTo(33583L);
+    }
+
+    @Test
+    public void processFuelNeedsGivenChallengeFileThen3393938() throws Exception {
+        // Given
+        String inputFilePath = "./src/test/resources/Day1/Day1Test.data";
+        Day1 day = new Day1();
+
+        // When
+        Long fuelNeeds = day.processFuelNeeds(inputFilePath);
+
+        // Then
+        assertThat(fuelNeeds).isNotNull();
+        assertThat(fuelNeeds).isEqualTo(3393938L);
     }
 }
