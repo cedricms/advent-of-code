a3e8584325adbfa77591ffa8d108f6e16fd6c62e
cedricms <cedricms@gmail.com>
2019-12-10 13:57:13 +0000

    Solves part 2 of Day4.

--- 2019/src/main/java/com/adventofcode/challenge2019/Day4.java
+++ 2019/src/main/java/com/adventofcode/challenge2019/Day4.java
@@ -1,8 +1,11 @@
 package com.adventofcode.challenge2019;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day4 extends Day {
 
-    public int getNumberOfPasswords(String input) {
+    public int getNumberOfPasswordsPart1(String input) {
         String[] inputValues = input.split("-");
 
         String minString = inputValues[0];
@@ -13,7 +16,7 @@ public class Day4 extends Day {
         int result = 0;
 
         for(int i = minInt; i <= maxInt; i++) {
-            if (isPasswordValid(Integer.toString(i))) {
+            if (isPasswordValidPart1(Integer.toString(i))) {
                 result++;
             }
         }
@@ -21,7 +24,7 @@ public class Day4 extends Day {
         return result;
     }
 
-    public boolean isPasswordValid(String password) {
+    public boolean isPasswordValidPart1(String password) {
         if (password != null
             && password.length() == 6) {
                 String previousValue = null;
@@ -47,6 +50,63 @@ public class Day4 extends Day {
 
                 return doubleAdjacentDigits;
             }
+
+        return false;
+    }
+
+    public int getNumberOfPasswordsPart2(String input) {
+        String[] inputValues = input.split("-");
+
+        String minString = inputValues[0];
+        int minInt = Integer.parseInt(minString);
+        String maxString = inputValues[1];
+        int maxInt = Integer.parseInt(maxString);
+
+        int result = 0;
+
+        for(int i = minInt; i <= maxInt; i++) {
+            if (isPasswordValidPart2(Integer.toString(i))) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isPasswordValidPart2(String password) {
+        if (password != null
+            && password.length() == 6) {
+            String previousValue = null;
+            int multipleAdjacentDigits = 1;
+            Map<Integer, Integer> multipleAdjacentDigitsMap = new HashMap<>();
+
+            for(int i = 0; i < password.length(); i++) {
+                String cursor = password.substring(i, i + 1);
+
+                int intCursor = Integer.parseInt(cursor);
+
+                if (previousValue != null) {
+                    int intPreviousValue = Integer.parseInt(previousValue);
+
+                    if (intPreviousValue == intCursor) {
+                        multipleAdjacentDigits++;
+                        multipleAdjacentDigitsMap.put(intCursor, multipleAdjacentDigits);
+                    } else if (intPreviousValue > intCursor) {
+                        return false;
+                    } else {
+                        multipleAdjacentDigits = 1;
+                    }
+                }
+
+                previousValue = cursor;
+            }
+
+            for (Map.Entry<Integer, Integer> value : multipleAdjacentDigitsMap.entrySet()) {
+                if (value.getValue() == 2) {
+                    return true;
+                }
+            }
+        }
 
         return false;
     }
--- 2019/src/test/java/com/adventofcode/challenge2019/Day4Test.java
+++ 2019/src/test/java/com/adventofcode/challenge2019/Day4Test.java
@@ -7,93 +7,158 @@ import static org.assertj.core.api.Assertions.*;
 public class Day4Test {
 
     @Test
-    public void isPasswordValidGiven111111ThenTrue() throws Exception {
+    public void isPasswordValidPart1Given111111ThenTrue() throws Exception {
         // Given
         String input = "111111";
         Day4 day = new Day4();
 
         // When
-        boolean result = day.isPasswordValid(input);
+        boolean result = day.isPasswordValidPart1(input);
 
         // Then
         assertThat(result).isTrue();
     }
 
     @Test
-    public void isPasswordValidGiven223450ThenFalse() throws Exception {
+    public void isPasswordValidPart1Given223450ThenFalse() throws Exception {
         // Given
         String input = "223450";
         Day4 day = new Day4();
 
         // When
-        boolean result = day.isPasswordValid(input);
+        boolean result = day.isPasswordValidPart1(input);
 
         // Then
         assertThat(result).isFalse();
     }
 
     @Test
-    public void isPasswordValidGiven123789ThenFalse() throws Exception {
+    public void isPasswordValidPart1Given123789ThenFalse() throws Exception {
         // Given
         String input = "123789";
         Day4 day = new Day4();
 
         // When
-        boolean result = day.isPasswordValid(input);
+        boolean result = day.isPasswordValidPart1(input);
 
         // Then
         assertThat(result).isFalse();
     }
 
     @Test
-    public void getNumberOfPasswordsGiven123787_123794Then1() throws Exception {
+    public void getNumberOfPasswordsPart1Given123787_123794Then1() throws Exception {
         // Given
         String input = "123787-123794";
         Day4 day = new Day4();
 
         // When
-        int result = day.getNumberOfPasswords(input);
+        int result = day.getNumberOfPasswordsPart1(input);
 
         // Then
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void getNumberOfPasswordsGiven123787_123802Then2() throws Exception {
+    public void getNumberOfPasswordsPart1Given123787_123802Then2() throws Exception {
         // Given
         String input = "123787-123802";
         Day4 day = new Day4();
 
         // When
-        int result = day.getNumberOfPasswords(input);
+        int result = day.getNumberOfPasswordsPart1(input);
 
         // Then
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    public void getNumberOfPasswordsGiven100001_100100Then17() throws Exception {
+    public void getNumberOfPasswordsPart1Given100001_100100Then17() throws Exception {
         // Given
         String input = "111111-111130";
         Day4 day = new Day4();
 
         // When
-        int result = day.getNumberOfPasswords(input);
+        int result = day.getNumberOfPasswordsPart1(input);
 
         // Then
         assertThat(result).isEqualTo(17);
     }
 
     @Test
-    public void getNumberOfPasswordsGiven264360_746325Then945() throws Exception {
+    public void getNumberOfPasswordsPart1Given264360_746325Then945() throws Exception {
         // Given
         String input = "264360-746325";
         Day4 day = new Day4();
 
         // When
-        int result = day.getNumberOfPasswords(input);
+        int result = day.getNumberOfPasswordsPart1(input);
 
         // Then
         assertThat(result).isEqualTo(945);
+    }
+
+    @Test
+    public void isPasswordValidPart2Given112233ThenTrue() throws Exception {
+        // Given
+        String input = "112233";
+        Day4 day = new Day4();
+
+        // When
+        boolean result = day.isPasswordValidPart2(input);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void isPasswordValidPart2Given123444ThenFalse() throws Exception {
+        // Given
+        String input = "123444";
+        Day4 day = new Day4();
+
+        // When
+        boolean result = day.isPasswordValidPart2(input);
+
+        // Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void isPasswordValidPart2Given111122ThenTrue() throws Exception {
+        // Given
+        String input = "111122";
+        Day4 day = new Day4();
+
+        // When
+        boolean result = day.isPasswordValidPart2(input);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void isPasswordValidPart2Given112223ThenTrue() throws Exception {
+        // Given
+        String input = "112223";
+        Day4 day = new Day4();
+
+        // When
+        boolean result = day.isPasswordValidPart2(input);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void getNumberOfPasswordsPart2Given264360_746325Then617() throws Exception {
+        // Given
+        String input = "264360-746325";
+        Day4 day = new Day4();
+
+        // When
+        int result = day.getNumberOfPasswordsPart2(input);
+
+        // Then
+        assertThat(result).isEqualTo(617);
     }
 }
